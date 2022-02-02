cea0374712f055638db6277e67c5772aaf1d49f3
Corleone <Corleone@MacBook-Air-Andrej.local>
2022-02-02 21:13:16 +0200

    added burger menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
 
-const snake = document.querySelector(".head__snake");
 const vector = document.querySelector(".head__logo");
 const name = document.querySelector(".head__name");
-const text = document.querySelector(".head__menu");
 let menuBtn = document.querySelector('.head__menu-btn');
 let menu = document.querySelector('.head__menu');
+let list = document.querySelector('.head__list');
+let link = document.querySelectorAll('.head__link');
+
 
 window.addEventListener("scroll", function() {
     checkScroll();
@@ -14,16 +15,33 @@ document.addEventListener("DOMContentLoaded", function (){
     checkScroll();
 });
 
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+
 function checkScroll() {
     let scrollPos = window.scrollY;
 
     if (scrollPos > 0) {
-        // vector.remove('head__logo');
-        // // name.remove('head__name');
-        vector.classList.add('display');
-    } else {
+        vector.classList.add('head__display');
+        name.classList.add('head__display');
+        menu.classList.add('head__menu-right');
+        menuBtn.classList.add('head__menu-btnRight');
+        list.classList.add('head__list-block');
 
+        for (let i = 0; i <= link.length; i++) {
+            link[i].classList.add('head__link-block');
+        }
+    } else {
+        vector.classList.remove('head__display');
+        name.classList.remove('head__display');
+        menu.classList.remove('head__menu-right');
+        menuBtn.classList.remove('head__menu-btnRight');
+        list.classList.remove('head__list-block');
+        link.classList.remove('head__link-block');
     }
 }
+
 
 
